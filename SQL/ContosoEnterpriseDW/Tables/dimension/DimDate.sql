87158fed8fb2c50c29a60560e7671557f4371160
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-02 19:45:18 -0500

    Creating Dimdate table

---
+++ SQL/ContosoEnterpriseDW/Tables/dimension/DimDate.sql
@@ -0,0 +1,20 @@
+﻿
+CREATE TABLE [dimension].[DimDate]
+(
+    [DateKey]           INT             NOT NULL,
+    [FullDate]          DATE            NOT NULL,
+    [CalendarYear]      SMALLINT        NOT NULL,
+    [CalendarQuarter]   TINYINT         NOT NULL,
+    [MonthNumber]       TINYINT         NOT NULL,
+    [MonthName]         NVARCHAR(20)    NOT NULL,
+    [DayOfMonth]        TINYINT         NOT NULL,
+    [DayOfWeek]         TINYINT         NOT NULL,
+    [DayName]           NVARCHAR(20)    NOT NULL,
+    [IsWeekend]         BIT             NOT NULL,
+
+    CONSTRAINT [PK_DimDate]
+        PRIMARY KEY CLUSTERED ([DateKey] ASC),
+    CONSTRAINT [UQ_DimDate_FullDate]
+        UNIQUE NONCLUSTERED ([FullDate])
+);
+GO
