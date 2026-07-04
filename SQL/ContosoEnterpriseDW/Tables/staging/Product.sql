14dbef99dace295432804dded3b07e9a6e890e09
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-04 09:11:58 -0500

    Created Staging Product table

---
+++ SQL/ContosoEnterpriseDW/Tables/staging/Product.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [staging].[Product]
+(
+    ProductAlternateKey NVARCHAR(50) NOT NULL,
+
+    ProductName NVARCHAR(200) NOT NULL,
+
+    ProductCategory NVARCHAR(100) NULL,
+
+    ProductSubcategory NVARCHAR(100) NULL,
+
+    Brand NVARCHAR(100) NULL,
+
+    Color NVARCHAR(50) NULL,
+
+    Size NVARCHAR(50) NULL,
+
+    StandardCost DECIMAL(18,2) NULL,
+
+    ListPrice DECIMAL(18,2) NULL,
+
+    IsActive BIT NOT NULL
+);
+GO
