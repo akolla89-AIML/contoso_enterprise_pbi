cd2715b0c111b6730d69cfc4f2f2961c8335e060
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-04 11:51:19 -0500

    Create Metadata (reusable ETL) framework

---
+++ SQL/ContosoEnterpriseDW/Tables/metadata/ETLBatch.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [metadata].[ETLBatch]
+(
+    ETLBatchKey INT IDENTITY(1,1) NOT NULL,
+
+    BatchName NVARCHAR(200) NOT NULL,
+
+    BatchStartTime DATETIME2 NOT NULL,
+
+    BatchEndTime DATETIME2 NULL,
+
+    BatchStatus NVARCHAR(20) NOT NULL,
+
+    InitiatedBy NVARCHAR(100) NOT NULL,
+
+    CONSTRAINT PK_ETLBatch
+        PRIMARY KEY CLUSTERED (ETLBatchKey)
+);
+GO
