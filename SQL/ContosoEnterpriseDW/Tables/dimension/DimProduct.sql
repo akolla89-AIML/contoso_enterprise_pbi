3bd1dad6ec7a33e07675a2e60f6ffcad09afdf65
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-04 08:41:40 -0500

    Created DimProduct table

---
+++ SQL/ContosoEnterpriseDW/Tables/dimension/DimProduct.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE [dimension].[DimProduct]
+(
+    ProductKey INT IDENTITY(1,1) NOT NULL,
+
+    ProductAlternateKey NVARCHAR(50) NOT NULL,
+
+    ProductName NVARCHAR(200) NOT NULL,
+
+    ProductCategory NVARCHAR(100) NULL,
+
+    ProductSubcategory NVARCHAR(100) NULL,
+
+    Brand NVARCHAR(100) NULL,
+
+    Color NVARCHAR(50) NULL,
+
+    Size NVARCHAR(50) NULL,
+
+    StandardCost DECIMAL(18,2) NULL,
+
+    ListPrice DECIMAL(18,2) NULL,
+
+    IsActive BIT NOT NULL
+        CONSTRAINT DF_DimProduct_IsActive DEFAULT (1),
+
+    CreatedDate DATETIME2 NOT NULL
+        CONSTRAINT DF_DimProduct_CreatedDate DEFAULT (SYSUTCDATETIME()),
+
+    ModifiedDate DATETIME2 NOT NULL
+        CONSTRAINT DF_DimProduct_ModifiedDate DEFAULT (SYSUTCDATETIME()),
+
+    CONSTRAINT PK_DimProduct
+        PRIMARY KEY CLUSTERED (ProductKey)
+);
+GO
