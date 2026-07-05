be4a681015e4b54b43e889a985e00f2262425870
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-05 08:48:48 -0500

    Create Customer sales view reporting view

---
+++ SQL/ContosoEnterpriseDW/Views/vwCustomerSales.sql
@@ -0,0 +1,50 @@
+﻿CREATE VIEW [dbo].[vwCustomerSales]
+AS
+
+SELECT
+
+    c.CustomerAlternateKey,
+
+    c.FullName,
+
+    c.Gender,
+
+    c.City,
+
+    c.StateProvince,
+
+    c.Country,
+
+    COUNT(*) AS NumberOfSales,
+
+    SUM(f.OrderQuantity) AS TotalQuantity,
+
+    SUM(f.SalesAmount) AS TotalSales,
+
+    SUM(f.DiscountAmount) AS TotalDiscount,
+
+    SUM(f.TotalProductCost) AS TotalCost,
+
+    SUM(f.ProfitAmount) AS TotalProfit,
+
+    AVG(f.SalesAmount) AS AverageOrderValue
+
+FROM fact.FactSales AS f
+
+INNER JOIN dimension.DimCustomer AS c
+    ON f.CustomerKey = c.CustomerKey
+
+GROUP BY
+
+    c.CustomerAlternateKey,
+
+    c.FullName,
+
+    c.Gender,
+
+    c.City,
+
+    c.StateProvince,
+
+    c.Country;
+GO
