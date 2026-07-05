bb837aa4cf226443005ffa29491f0b94736258b0
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-05 08:28:54 -0500

    Create Product sales view reporting view

---
+++ SQL/ContosoEnterpriseDW/Views/vwProductSales.sql
@@ -0,0 +1,46 @@
+﻿CREATE VIEW [dbo].[vwProductSales]
+AS
+
+SELECT
+
+    p.ProductAlternateKey,
+
+    p.ProductName,
+
+    p.ProductCategory,
+
+    p.ProductSubcategory,
+
+    p.Brand,
+
+    COUNT(*) AS NumberOfSales,
+
+    SUM(f.OrderQuantity) AS TotalQuantity,
+
+    SUM(f.SalesAmount) AS TotalSales,
+
+    SUM(f.DiscountAmount) AS TotalDiscount,
+
+    SUM(f.TotalProductCost) AS TotalCost,
+
+    SUM(f.ProfitAmount) AS TotalProfit,
+
+    AVG(f.UnitPrice) AS AverageSellingPrice
+
+FROM fact.FactSales AS f
+
+INNER JOIN dimension.DimProduct AS p
+    ON f.ProductKey = p.ProductKey
+
+GROUP BY
+
+    p.ProductAlternateKey,
+
+    p.ProductName,
+
+    p.ProductCategory,
+
+    p.ProductSubcategory,
+
+    p.Brand;
+GO
