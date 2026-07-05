b937dc8eac8c60f63e469b928a0f33c8d88b0d07
akolla89-AIML <abhikolla.ak89@gmail.com>
2026-07-05 08:10:54 -0500

    Create Daily sales view reporting view

---
+++ SQL/ContosoEnterpriseDW/Views/vwDailysales.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [dbo].[vwDailySales]
+AS
+
+SELECT
+
+    d.FullDate,
+
+    COUNT(*) AS NumberOfSales,
+
+    SUM(f.OrderQuantity) AS TotalQuantity,
+
+    SUM(f.SalesAmount) AS TotalSales,
+
+    SUM(f.DiscountAmount) AS TotalDiscount,
+
+    SUM(f.TotalProductCost) AS TotalCost,
+
+    SUM(f.ProfitAmount) AS TotalProfit
+
+FROM fact.FactSales f
+
+INNER JOIN dimension.DimDate d
+    ON f.DateKey = d.DateKey
+
+GROUP BY
+
+    d.FullDate;
+GO
